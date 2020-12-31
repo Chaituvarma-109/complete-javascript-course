4621fd2ce77a81b1f8046ffc80e71dd5b28c2a9c
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2020-12-31 14:17:58 +0530

    added the tutorial in FJS-2

--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -64,3 +64,11 @@ const calcAge2 = birthYear => 2037 - birthYear;  // This function is called as A
 
 console.log(calcAge2(1992));
 
+const yearsUntilRetirement = (birthYear, firstName) => {
+    const age = 2037- birthYear;
+    const retirement = 65 - age;
+    // return retirement;
+    return `${firstName} retires in ${retirement} years.`
+}
+
+console.log(yearsUntilRetirement(1992, 'chaitu'));
