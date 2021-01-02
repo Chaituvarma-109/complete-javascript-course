ccde696c9174e533fb82133d64665b7bfd81d201
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2021-01-02 19:43:51 +0530

    Added assignments and tutorial notes.

--- 02-Fundamentals-Part-2/starter/Assignments.js
+++ 02-Fundamentals-Part-2/starter/Assignments.js
@@ -1,2 +1,19 @@
-'use strict'
+// 'use strict'
 
+// const calcAverage = (score1, score2, score3) => {
+//     return (score1+score2+score3)/3;
+// } 
+
+// const checkWinner = (avgDolphins, avgKoalas) => {
+//     if (avgDolphins >= (2*avgKoalas)) {
+//         return "Dolphins";
+//     }
+//     return "Koalas"
+// } 
+
+// let dolphinsAvg = calcAverage(85,54,41);
+// let koalasAvg = calcAverage(23,34,27);
+
+// let winningTeam = checkWinner(dolphinsAvg, koalasAvg);
+
+// console.log(`${winningTeam} win ${dolphinsAvg} vs ${koalasAvg}`);
--- 02-Fundamentals-Part-2/starter/script.js
+++ 02-Fundamentals-Part-2/starter/script.js
@@ -75,17 +75,75 @@
 
 // function calling from another function
 
-function cutFruitPieces(fruit) {
-    return fruit * 4;
-};
+// function cutFruitPieces(fruit) {
+//     return fruit * 4;
+// };
 
-function fruitProcessor(apples, oranges) {
+// function fruitProcessor(apples, oranges) {
 
-    const applePieces =  cutFruitPieces(apples);
-    const orangePieces = cutFruitPieces(oranges);
+//     const applePieces =  cutFruitPieces(apples);
+//     const orangePieces = cutFruitPieces(oranges);
 
-    const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
-    return juice;
-};
+//     const juice = `Juice with ${applePieces} apple pieces and ${orangePieces} orange pieces.`;
+//     return juice;
+// };
 
-console.log(fruitProcessor(2,3));
+// console.log(fruitProcessor(2,3));
+
+// Arrays
+
+// const friends = ['chaitu','bunny','vasu'];
+
+// console.log(friends)
+
+// console.log(friends[2]);
+
+// console.log('length of friends:', friends.length);
+
+// const years = new Array(1992, 2020, 1985);
+
+// console.log(years);
+
+// console.log(years[1]);
+
+// const age = new Array(22,34,'varma');
+
+// console.log(age);
+
+// friends[2] = 'Dinesh';
+
+// friends[3] = 'Bhanu';
+
+// console.log(friends);
+
+// let city = 'bhimavaram';
+
+// const chaitu = ['Indra', 2021, 'jan', friends,'schmit',1864,city];
+
+// console.log(chaitu);
+
+// const calcAge = function(birthYear) {
+//     return birthYear - 2024;
+// }
+
+// const years = [1991,1885,1794,200];
+
+// let res = [calcAge(years[0]), calcAge(years[1]), calcAge(years[2])];
+
+// console.log(res);
+
+const friends = ['chaitu', 'bunny', 'vasu'];
+const newLength = friends.push('veera');
+friends.push('rakesh');
+
+console.log(friends);
+console.log(newLength);
+
+friends.unshift('john');
+
+console.log(friends);
+
+friends.pop();
+const poppedEle = friends.pop();
+console.log(friends);
+console.log(poppedEle);
