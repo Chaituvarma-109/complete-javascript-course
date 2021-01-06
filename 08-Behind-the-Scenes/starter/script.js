fbaf992518625e774e37915927eb4317edacfe42
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2021-01-06 14:43:16 +0530

    Scope tutorial example programmes

--- 08-Behind-the-Scenes/starter/script.js
+++ 08-Behind-the-Scenes/starter/script.js
@@ -1,1 +1,27 @@
 'use strict';
+
+function calcAge(birthYear) {
+  const age = 2037 - birthYear;
+
+  function printAge() {
+    const output = `${firstName}, you are ${age}, born in ${birthYear}`;
+    console.log(output);
+
+    if (birthYear >= 1981 && birthYear <= 1996) {
+      var millenial = true;
+      const str = `${firstName} you are a millenial.`;
+      console.log(str);
+
+      function add(a, b) {
+        return a + b;
+      }
+    }
+    console.log(millenial);
+    add(2, 3);
+  }
+  printAge();
+  return age;
+}
+
+const firstName = 'Chaitu';
+calcAge(1992);
