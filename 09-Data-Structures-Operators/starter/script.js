969afcc2af94eb7c8c77ea3f5b79520133a5ab21
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2021-02-01 11:18:17 +0530

    added lecture scripts.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -1,5 +1,14 @@
 'use strict';
 
+const arr = [2, 3, 4];
+let aa = arr[0];
+let bb = arr[1];
+let cc = arr[2];
+console.log(aa, bb, cc);
+
+const [x, y, z] = arr;
+console.log(x, y, z);
+
 const restaurant = {
   name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -21,4 +30,33 @@ const restaurant = {
       close: 24,
     },
   },
+  order: function (startIndex, mainIndex) {
+    return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
+  },
 };
+
+// const [first, second] = restaurant.categories;
+// console.log(first, second);
+
+const [first, , , second] = restaurant.categories;
+console.log(first, second);
+
+let [main, , secondary] = restaurant.categories;
+console.log(main, secondary);
+
+// swaping two elements
+[main, secondary] = [secondary, main];
+console.log(main, secondary);
+
+// destructuring return array elements
+const [item1, item2] = restaurant.order(2, 0);
+console.log(item1, item2);
+
+// Destructuring nested elements.
+const nested = [1, 2, [3, 4]];
+const [a, b, [c, d]] = nested;
+console.log(a, b, c, d);
+
+// default values
+const [p = 1, q = 1, r = 1] = [3, 44];
+console.log(p, q, r);
