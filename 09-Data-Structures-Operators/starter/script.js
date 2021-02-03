55ecbf4b243405f954fe68000a2ed6bf98f7585d
chaituvarma-109 <chaitanyavarma.u@gmail.com>
2021-02-03 16:12:23 +0530

    added looping, object literals, chaining.

--- 09-Data-Structures-Operators/starter/script.js
+++ 09-Data-Structures-Operators/starter/script.js
@@ -9,6 +9,23 @@
 // const [x, y, z] = arr;
 // console.log(x, y, z);
 
+// Object Literals Enhancement.
+const weekDays = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+const openingHours = {
+  [weekDays[3]]: {
+    open: 12,
+    close: 22,
+  },
+  [weekDays[4]]: {
+    open: 11,
+    close: 23,
+  },
+  [weekDays[5]]: {
+    open: 0, // Open 24 hours
+    close: 24,
+  },
+};
+
 const restaurant = {
   Name: 'Classico Italiano',
   location: 'Via Angelo Tavanti 23, Firenze, Italy',
@@ -16,23 +33,15 @@ const restaurant = {
   starterMenu: ['Focaccia', 'Bruschetta', 'Garlic Bread', 'Caprese Salad'],
   mainMenu: ['Pizza', 'Pasta', 'Risotto'],
 
-  openingHours: {
-    thu: {
-      open: 12,
-      close: 22,
-    },
-    fri: {
-      open: 11,
-      close: 23,
-    },
-    sat: {
-      open: 0, // Open 24 hours
-      close: 24,
-    },
-  },
-  order: function (startIndex, mainIndex) {
+  // ES6 Enhanced Object Literals
+  openingHours,
+
+  // ES6 functions in Objects.
+  order(startIndex, mainIndex) {
     return [this.starterMenu[startIndex], this.mainMenu[mainIndex]];
   },
+
+  // Before ES6 functions in objects
   orderDelivery: function ({
     starterIndex = 1,
     mainIndex = 0,
@@ -43,15 +52,92 @@ const restaurant = {
       `Order received! ${this.starterMenu[starterIndex]} and ${this.mainMenu[mainIndex]} will be delivered to ${address} at ${time}`
     );
   },
-  orderingPasta: function (ing1, ing2, ing3) {
+
+  orderingPasta(ing1, ing2, ing3) {
     console.log(`Here is your delecious pasta with ${ing1} ${ing2} ${ing3}`);
   },
-  orderingPizza: function (mainIngredient, ...otherIngredients) {
+
+  orderingPizza(mainIngredient, ...otherIngredients) {
     console.log(mainIngredient);
     console.log(otherIngredients);
   },
 };
 
+// // Looping over objects using keys.
+// const propKeys = Object.keys(restaurant);
+// console.log(propKeys);
+
+// for (const restaurant_details of Object.keys(restaurant)) {
+//   console.log(restaurant_details);
+// }
+
+// // Looping over objects using values.
+// const propValues = Object.values(restaurant);
+// console.log(propValues);
+
+// for (const restaurant_values of Object.values(restaurant)) {
+//   console.log(restaurant_values);
+// }
+
+// // Looping over objects using entries.
+// const propEntries = Object.entries(restaurant);
+// console.log(propEntries);
+
+// // example looping over objects
+// const openEntries = Object.entries(openingHours);
+
+// for (const [key, { open, close }] of openEntries) {
+//   console.log(`we open ${key} at ${open} and closes on ${close}`);
+// }
+
+// Before optional chaining.
+// if (restaurant.openingHours.mon) console.log(restaurant.openingHours.mon.open);
+
+// if (restaurant.openingHours.fri) console.log(restaurant.openingHours.fri.open);
+
+// if (restaurant.openingHours && restaurant.openingHours.mon)
+//   console.log(restaurant.openingHours.mon.open);
+
+// // After optional chaining.
+// console.log(restaurant.openingHours.mon?.open);
+
+// console.log(restaurant.openingHours?.mon?.open);
+// console.log(restaurant.openingHours?.fri?.open);
+
+// // real world example
+// const days = ['mon', 'tue', 'wed', 'thu', 'fri', 'sat', 'sun'];
+// for (const day of days) {
+//   const openHour = restaurant.openingHours[day]?.open ?? `Closed for this day`;
+//   console.log(openHour);
+// }
+
+// // Optional Chaining for Objects
+// console.log(restaurant.order?.(0, 1) ?? 'Method does not Exist.');
+// restaurant.orderDelivery?.(
+//   {
+//     time: '10',
+//     address: 'india',
+//     mainIndex: 0,
+//     starterIndex: 1,
+//   } ?? 'Method does not Exist.'
+// );
+
+// // Optional Chaining for Arrays
+// const personArr = [
+//   {
+//     name: 'chaitu',
+//     email: 'xyz@email.com',
+//   },
+//   {
+//     name: 'rakesh',
+//     email: 'zoo@zoom.com',
+//   },
+// ];
+
+// console.log(personArr[0]?.name ?? 'name does not exist.');
+// console.log(personArr[2]?.name ?? 'name does not exist.');
+
+// Destructuring Arrays.
 // restaurant.orderDelivery({
 //   time: '23:30',
 //   address: 'Via del Sole, 21',
@@ -190,3 +276,55 @@ const restaurant = {
 
 // // example
 // restaurant.orderingPizza('Mushrooms', 'Onions', 'Olives', 'spinach');
+
+// short-circuiting
+
+// Short-Circuiting using OR.
+// console.log(3 || 'jonas');
+// console.log('' || 'jonas');
+// console.log(true || 'jonas');
+// console.log(undefined || null);
+
+// console.log(undefined || 0 || '' || 'Hello' || 23 || null);
+
+// restaurant.numGuests = 23;
+// const guests1 = restaurant.numGuests ? restaurant.numGuests : 10;
+// console.log(guests1);
+
+// const guests2 = restaurant.numGuests || 10;
+// console.log(guests2);
+
+// // short-Circuiting using AND.
+// console.log(1 && restaurant.numGuests);
+// console.log(0 && 'jonas');
+// console.log(7 && 'jonas');
+
+// console.log('hello' && 23 && null && 'jonas');
+
+// // practical example
+// restaurant.orderingPizza && restaurant.orderingPizza('mushrooms', 'spinach');
+
+// Null Coalescing Operator
+// restaurant.numGuests = 0;
+// const guests = restaurant.numGuests || 10;
+// console.log(guests);
+
+// const correctGuests = restaurant.numGuests ?? 10;
+// console.log(correctGuests);
+
+// For-of Loop
+// const menu = [...restaurant.mainMenu, ...restaurant.starterMenu];
+// for (const item of menu) {
+//   console.log(item);
+// }
+// const arr = [1, 2, 3, 4, 5, 6, 7, 8, 9];
+// for (const num of arr) {
+//   if (num % 2 === 0) {
+//     console.log(num);
+//   }
+// }
+
+// for (const [i, item] of menu.entries()) {
+//   console.log(`${i + 1}: ${item}`);
+// }
+// console.log(menu.entries());
